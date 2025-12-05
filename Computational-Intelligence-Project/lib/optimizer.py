a921d34c0acfed9ac3f63e48720ab18a0c0271f0
nariimannn <nareemann00@gmail.com>
2025-12-05 23:48:50 +0200

    updated code

--- Computational-Intelligence-Project/lib/optimizer.py
+++
@@ -1,7 +0,0 @@
-class SGD:
-    def __init__(self, learning_rate=0.01):
-        self.learning_rate = learning_rate
-
-    def step(self, parameter, gradient):
-        # returns updated parameter (do not update in-place here)
-        return parameter - self.learning_rate * gradient
